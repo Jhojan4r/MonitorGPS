72332ef48bad53869e4a1b6a2c2a8f08881be88f
jhojan <jhojandev1@gmail.com>
2026-08-21 22:55:40 -0500

    Agregar seguimiento GPS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,20 @@ app = Flask(__name__)
 @app.route("/")
 def inicio():
     return render_template("mapa.html")
+
+
+@app.route("/ubicacion", methods=["POST"])
+def recibir_ubicacion():
+
+    datos = request.get_json()
+
+    print("📍 Ubicación recibida:")
+    print(datos)
+
+    return jsonify({
+        "ok": True,
+        "mensaje": "Ubicación recibida correctamente"
+    })
 
 
 if __name__ == "__main__":
